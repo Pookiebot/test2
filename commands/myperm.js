f79f38705609226ae237f672f3e1a319aea92001
Pookiebot <61335384+Pookiebot@users.noreply.github.com>
2020-04-11 13:54:48 +0200

    Update myperm.js

--- commands/myperm.js
+++ commands/myperm.js
@@ -1,19 +1,84 @@
+/* eslint-disable */
+
 const Command = require("../modules/Command.js");
 
-class MyPerm extends Command {
+class Help extends Command {
   constructor(client) {
     super(client, {
-      name: "myperm",
-      description: " Affiche votre niveau de permission.",
-      usage: "myperm",
-      guildOnly: true
+      name: "help",
+      description: "Afficher toutes les commandes disponibles.",
+      usage: "help [commande]",
+      aliases: ["h"]
     });
   }
 
-  run(message, args, level) {
-    const perm = this.client.config.permlevels.find(l => l.level === level).name;
-    message.reply(`ton niveau de permission est: ${level} - ${perm}`);
+  async run(message, args, level) {
+    try {
+      message.delete()
+      if (!args[0]) {
+        const settings = message.settings;
+
+        const myCommands = message.guild
+          ? this.client.commands.filter(
+              cmd => this.client.levelCache[cmd.conf.permLevel] <= level
+            )
+          : this.client.commands.filter(
+              cmd =>
+                this.client.levelCache[cmd.conf.permLevel] <= level &&
+                cmd.conf.guildOnly !== true
+            );
+
+        const commandNames = myCommands.keyArray();
+        const longest = commandNames.reduce(
+          (long, str) => Math.max(long, str.length),
+          0
+        );
+        let currentCat = "";
+        let output = `= Liste des commandes =\n\n[Utiliser ${
+          this.client.config.settings.prefix = settings.prefix
+        }help <nom de la commande> pour plus de détails]\n`;
+        const sorted = myCommands
+          .array()
+          .sort((p, c) =>
+            p.help.category > c.help.category
+              ? 1
+              : p.help.name > c.help.name && p.help.category === c.help.category
+              ? 1
+              : -1
+          );
+        sorted.forEach(c => {
+          const cat = c.help.category;
+          if (currentCat !== cat) {
+            output += `\u200b\n== ${cat} ==\n`;
+            currentCat = cat;
+          }
+          output += `${settings.prefix}${c.help.name}${" ".repeat(
+            longest - c.help.name.length
+          )} :: ${c.help.description}\n`;
+        });
+        message.channel.send(output, {
+          code: "asciidoc",
+          split: { char: "\u200b" }
+        });
+      } else {
+        let command = args[0];
+        if (this.client.commands.has(command)) {
+          command = this.client.commands.get(command);
+          if (level < this.client.levelCache[command.conf.permLevel]) return;
+          message.channel.send(
+            `= ${command.help.name} = \ndescription:: ${
+              command.help.description
+            }\nutilisation:: ${
+              command.help.usage
+            }\nalias:: ${command.conf.aliases.join(", ")}`,
+            { code: "asciidoc" }
+          );
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
-module.exports = MyPerm;
+module.exports = Help;
