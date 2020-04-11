44ed45db48e99bd88b40ba39a6e3a39257e74bb7
Pookiebot <61335384+Pookiebot@users.noreply.github.com>
2020-04-11 18:57:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const { Client, Collection} = require("discord.js");
-const { promisify} = require ("util");
+const { Client, Collection } = require("discord.js");
+const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
 const klaw = require("klaw");
 const path = require("path");
+const mysql = require("mysql");
+const { getConfig } = require("./config.js")
 require("dotenv").config();
 
-class Gardien extends Client {
+class Flareon extends Client {
   constructor(options) {
     super(options);
-    this.config = require("./config.js");
 
+    
     this.commands = new Collection();
     this.aliases = new Collection();
-    
+    this.queue = new Map();
+
     this.settings = new Enmap({
       name: "settings",
+      cloneLevel: "deep",
+      fetchAll: false,
+      autoFetch: true
+    });
+    this.warns = new Enmap({
+      name: "warns",
       cloneLevel: "deep",
       fetchAll: false,
       autoFetch: true
@@ -27,10 +36,10 @@ class Gardien extends Client {
 
   // Permission
 
-  permlevel(message) {
+ async permlevel(message) {
     let permlvl = 0;
-
-    const permOrder = this.config.permLevels
+    const { permLevels } = await getConfig();
+    const permOrder = permLevels
       .slice(0)
       .sort((p, c) => (p.level < c.level ? 1 : -1));
 
@@ -45,9 +54,9 @@ class Gardien extends Client {
     return permlvl;
   }
 
-  loadComand(commandPath, commandName) {
+  loadCommand(commandPath, commandName) {
     try {
-      const props = new (require(`${commandPath}${path.step}${commandName}`))(
+      const props = new (require(`${commandPath}${path.sep}${commandName}`))(
         this
       );
       this.logger.log(`Chargement de la commande: ${props.help.name}`, "log");
@@ -61,12 +70,33 @@ class Gardien extends Client {
       });
       return false;
     } catch (e) {
-      return `impossible de charger la commande ${commandName} : ${e}`;
+      return `Impossible de charger la commande ${commandName}: ${e}`;
     }
   }
 
-  getSettings(guild) {
-    const defaults = this.config.dafaultSettings || {};
+  async unloadCommand(commandPath, commandName) {
+    let command;
+    if (this.command.has(commandName)) {
+      command = this.commands.get(commandName);
+    } else if (this.aliases.has(commandName)) {
+      command = this.commands.get(this.aliases.get(commandName));
+    }
+    if (!command)
+      return `La commande \`${commandName}\` ne semble pas exister. Essayez à nouveau!`;
+
+    if (command.shutdown) {
+      await command.shutdown(this);
+    }
+    delete require.cache[
+      require.resolve(`${commandPath}${path.sep}${commandName}.js`)
+    ];
+    return false;
+  }
+  
+
+   async getSettings(guild){
+    const { defaultSettings} = await getConfig();
+    const defaults = defaultSettings
     const guildData = this.settings.get(guild.id) || {};
     const returnObject = {};
     Object.keys(defaults).forEach(key => {
@@ -75,23 +105,25 @@ class Gardien extends Client {
     return returnObject;
   }
 }
+(async () => {
+const config = await getConfig;
+const client = new Flareon;
 
-const client = new Gardien();
-console.log(client.config.permLevels.map(p =>  `${p.level}: ${p.name}`));
+// Fonction d'initialisation
 
-// Fonction d'initialisation.
 const init = async () => {
   // Récupération des commandes
-  klaw("./commands").on("ata", item => {
+  klaw("./commands").on("data", item => {
     const cmdFile = path.parse(item.path);
-    if (cmdFile.ext || cmdFile.ext !== ".js") return;
-    const response = client .loadCommmand(cmdFile.dir, 
+    if (!cmdFile.ext || cmdFile.ext !== ".js") return;
+    const response = client.loadCommand(
+      cmdFile.dir,
       `${cmdFile.name}${cmdFile.ext}`
     );
     if (response) client.logger.error(response);
   });
 
-  // Récupération  des événements
+  // Récupération des événements
   const evtFiles = await readdir("./events");
   client.logger.log(`Chargement de ${evtFiles.length} événements.`, "log");
   evtFiles.forEach(file => {
@@ -101,14 +133,14 @@ const init = async () => {
     client.on(eventName, (...args) => event.run(...args));
     delete require.cache[require.resolve(`./events/${file}`)];
   });
-
-  client.levelCache ={};
-  for (let i = 0; i < client.config.permLevels.length; i++) {
-    const thisLevel = client.config.permLevels[i];
+  client.levelCache = {};
+  const { permLevels } = await getConfig()
+  for (let i = 0; i < permLevels; i++) {
+    const thisLevel = permLevels[i];
     client.levelCache[thisLevel.name] = thisLevel.level;
   }
 
   client.login(process.env.CLIENT_TOKEN);
-};
-
+}; 
 init();
+})();
